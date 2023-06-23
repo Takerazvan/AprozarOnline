248648128bfa311e82da4d2a1762cf64b28eedf5
Maria <simam9520@gmail.com>
2023-06-23 09:41:19 +0300

    fixed .gitignore

--- backend/src/main/java/com/codecool/backend/products/shoppingcart/ShoppingCartDTO.java
+++
@@ -1,8 +0,0 @@
-//package com.codecool.backend.products.shoppingcart;
-//
-//import com.codecool.backend.products.orders.CartItem;
-//
-//import java.util.List;
-//
-//public record ShoppingCartDTO(Double total, List<CartItem> items) {
-//}
